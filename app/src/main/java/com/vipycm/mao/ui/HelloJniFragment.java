d59855770cbab75910d98b0207e40547ba7463ea
mao <x@vipycm.com>
2017-04-06 19:34:52 +0800

    移除无用的import

--- app/src/main/java/com/vipycm/mao/ui/HelloJniFragment.java
+++ app/src/main/java/com/vipycm/mao/ui/HelloJniFragment.java
@@ -1,11 +1,9 @@
 package com.vipycm.mao.ui;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.vipycm.commons.MaoLog;
--- app/src/main/java/com/vipycm/mao/ui/PmFragment.java
+++ app/src/main/java/com/vipycm/mao/ui/PmFragment.java
@@ -8,11 +8,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.RemoteException;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.vipycm.commons.MaoLog;
