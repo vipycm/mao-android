378646d2fef7cd3bc6254e75869e1e83896f5a01
mao <x@vipycm.com>
2017-04-10 18:41:56 +0800

    修复拍照预览被 拉伸的问题

--- app/src/main/java/com/vipycm/mao/ui/CameraFragment.java
+++ app/src/main/java/com/vipycm/mao/ui/CameraFragment.java
@@ -2,8 +2,14 @@ package com.vipycm.mao.ui;
 
 import android.Manifest.permission;
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.graphics.ImageFormat;
+import android.graphics.Matrix;
+import android.graphics.Point;
+import android.graphics.RectF;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -13,26 +19,30 @@ import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.CaptureResult;
 import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.v4.app.ActivityCompat;
+import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.Surface;
-import android.view.TextureView;
 import android.view.TextureView.SurfaceTextureListener;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.vipycm.commons.MaoLog;
 import com.vipycm.mao.R;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * CameraFragment
@@ -46,8 +56,7 @@ public class CameraFragment extends MaoFragment implements SurfaceTextureListene
     private static final String LENS_FACING_FRONT = String.valueOf(CameraCharacteristics.LENS_FACING_FRONT);
     private static final String LENS_FACING_BACK = String.valueOf(CameraCharacteristics.LENS_FACING_BACK);
 
-    TextView txt_content;
-    TextureView ttv_camera;
+    AutoFitTextureView ttv_camera;
     ToggleButton tbtn_camera;
 
     CameraManager mCameraManager;
@@ -58,12 +67,24 @@ public class CameraFragment extends MaoFragment implements SurfaceTextureListene
     String mCurLens = LENS_FACING_BACK;
     CameraDevice mCurCameraDevice;
 
+    /**
+     * Max preview width that is guaranteed by Camera2 API
+     */
+    private static final int MAX_PREVIEW_WIDTH = 1920;
+
+    /**
+     * Max preview height that is guaranteed by Camera2 API
+     */
+    private static final int MAX_PREVIEW_HEIGHT = 1080;
+    /**
+     * The {@link android.util.Size} of camera preview.
+     */
+    private Size mPreviewSize;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         log.i("onCreateView");
         View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
-        txt_content = (TextView) rootView.findViewById(R.id.txt_content);
-        txt_content.setText(this.getClass().getSimpleName());
 
         mCameraManager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
 
@@ -71,7 +92,7 @@ public class CameraFragment extends MaoFragment implements SurfaceTextureListene
         mHandlerThread.start();
         mHandler = new Handler(mHandlerThread.getLooper());
 
-        ttv_camera = (TextureView) rootView.findViewById(R.id.ttv_camera);
+        ttv_camera = (AutoFitTextureView) rootView.findViewById(R.id.ttv_camera);
         ttv_camera.setSurfaceTextureListener(this);
 
         tbtn_camera = (ToggleButton) rootView.findViewById(R.id.tbtn_camera);
@@ -105,7 +126,7 @@ public class CameraFragment extends MaoFragment implements SurfaceTextureListene
 
     @Override
     public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-
+        configureTransform(width, height);
     }
 
     @Override
@@ -120,8 +141,8 @@ public class CameraFragment extends MaoFragment implements SurfaceTextureListene
 
     private void openCamera(String cameraId) {
         try {
-            CameraCharacteristics characteristics = mCameraManager.getCameraCharacteristics(cameraId);
-            characteristics.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL);
+//            CameraCharacteristics characteristics = mCameraManager.getCameraCharacteristics(cameraId);
+//            characteristics.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL);
             if (ActivityCompat.checkSelfPermission(getContext(), permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                 // TODO: Consider calling
                 //    ActivityCompat#requestPermissions
@@ -132,6 +153,8 @@ public class CameraFragment extends MaoFragment implements SurfaceTextureListene
                 // for ActivityCompat#requestPermissions for more details.
                 return;
             }
+            setUpCameraOutputs(cameraId, ttv_camera.getWidth(), ttv_camera.getHeight());
+            configureTransform(ttv_camera.getWidth(), ttv_camera.getHeight());
             mCameraManager.openCamera(cameraId, mCameraDeviceStateCallback, mHandler);
         } catch (CameraAccessException e) {
             e.printStackTrace();
@@ -145,9 +168,178 @@ public class CameraFragment extends MaoFragment implements SurfaceTextureListene
         }
     }
 
+    private void setUpCameraOutputs(String cameraId, int width, int height) {
+        Activity activity = getActivity();
+        CameraManager manager = (CameraManager) activity.getSystemService(Context.CAMERA_SERVICE);
+        try {
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+            StreamConfigurationMap streamCfgMap = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            if (streamCfgMap == null) {
+                return;
+            }
+            // Find out if we need to swap dimension to get the preview size relative to sensor
+            // coordinate.
+            int displayRotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+            // 相机的方向
+            int sensorOrientation = characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION);
+            boolean swappedDimensions = false;
+            switch (displayRotation) {
+                case Surface.ROTATION_0:
+                case Surface.ROTATION_180:
+                    if (sensorOrientation == 90 || sensorOrientation == 270) {
+                        //屏幕的方向和相机的方向十字交叉，比较宽高时需要转换
+                        swappedDimensions = true;
+                    }
+                    break;
+                case Surface.ROTATION_90:
+                case Surface.ROTATION_270:
+                    if (sensorOrientation == 0 || sensorOrientation == 180) {
+                        swappedDimensions = true;
+                    }
+                    break;
+                default:
+                    log.e("Display rotation is invalid: " + displayRotation);
+            }
+
+            Point displaySize = new Point();
+            activity.getWindowManager().getDefaultDisplay().getSize(displaySize);
+            int rotatedPreviewWidth = width;
+            int rotatedPreviewHeight = height;
+            int maxPreviewWidth = displaySize.x;
+            int maxPreviewHeight = displaySize.y;
+
+            if (swappedDimensions) {
+                rotatedPreviewWidth = height;
+                rotatedPreviewHeight = width;
+                maxPreviewWidth = displaySize.y;
+                maxPreviewHeight = displaySize.x;
+            }
+
+            if (maxPreviewWidth > MAX_PREVIEW_WIDTH) {
+                maxPreviewWidth = MAX_PREVIEW_WIDTH;
+            }
+
+            if (maxPreviewHeight > MAX_PREVIEW_HEIGHT) {
+                maxPreviewHeight = MAX_PREVIEW_HEIGHT;
+            }
+            Size largest = Collections.max(Arrays.asList(streamCfgMap.getOutputSizes(ImageFormat.JPEG)), new CompareSizesByArea());
+
+            mPreviewSize = chooseOptimalSize(streamCfgMap.getOutputSizes(SurfaceTexture.class),
+                    rotatedPreviewWidth, rotatedPreviewHeight, maxPreviewWidth,
+                    maxPreviewHeight, largest);
+
+            // We fit the aspect ratio of TextureView to the size of preview we picked.
+            int orientation = getResources().getConfiguration().orientation;
+            if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                ttv_camera.setAspectRatio(
+                        mPreviewSize.getWidth(), mPreviewSize.getHeight());
+            } else {
+                ttv_camera.setAspectRatio(mPreviewSize.getHeight(), mPreviewSize.getWidth());
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Configures the necessary {@link android.graphics.Matrix} transformation to `mTextureView`.
+     * This method should be called after the camera preview size is determined in
+     * setUpCameraOutputs and also the size of `mTextureView` is fixed.
+     *
+     * @param viewWidth  The width of `mTextureView`
+     * @param viewHeight The height of `mTextureView`
+     */
+    private void configureTransform(int viewWidth, int viewHeight) {
+        Activity activity = getActivity();
+        if (null == ttv_camera || null == mPreviewSize || null == activity) {
+            return;
+        }
+        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        Matrix matrix = new Matrix();
+        RectF viewRect = new RectF(0, 0, viewWidth, viewHeight);
+        RectF bufferRect = new RectF(0, 0, mPreviewSize.getHeight(), mPreviewSize.getWidth());
+        float centerX = viewRect.centerX();
+        float centerY = viewRect.centerY();
+        if (Surface.ROTATION_90 == rotation || Surface.ROTATION_270 == rotation) {
+            bufferRect.offset(centerX - bufferRect.centerX(), centerY - bufferRect.centerY());
+            matrix.setRectToRect(viewRect, bufferRect, Matrix.ScaleToFit.FILL);
+            float scale = Math.max(
+                    (float) viewHeight / mPreviewSize.getHeight(),
+                    (float) viewWidth / mPreviewSize.getWidth());
+            matrix.postScale(scale, scale, centerX, centerY);
+            matrix.postRotate(90 * (rotation - 2), centerX, centerY);
+        } else if (Surface.ROTATION_180 == rotation) {
+            matrix.postRotate(180, centerX, centerY);
+        }
+        ttv_camera.setTransform(matrix);
+    }
+
+    /**
+     * Given {@code choices} of {@code Size}s supported by a camera, choose the smallest one that
+     * is at least as large as the respective texture view size, and that is at most as large as the
+     * respective max size, and whose aspect ratio matches with the specified value. If such size
+     * doesn't exist, choose the largest one that is at most as large as the respective max size,
+     * and whose aspect ratio matches with the specified value.
+     *
+     * @param choices           The list of sizes that the camera supports for the intended output
+     *                          class
+     * @param textureViewWidth  The width of the texture view relative to sensor coordinate
+     * @param textureViewHeight The height of the texture view relative to sensor coordinate
+     * @param maxWidth          The maximum width that can be chosen
+     * @param maxHeight         The maximum height that can be chosen
+     * @param aspectRatio       The aspect ratio
+     * @return The optimal {@code Size}, or an arbitrary one if none were big enough
+     */
+    private static Size chooseOptimalSize(Size[] choices, int textureViewWidth,
+                                          int textureViewHeight, int maxWidth, int maxHeight, Size aspectRatio) {
+
+        // Collect the supported resolutions that are at least as big as the preview Surface
+        List<Size> bigEnough = new ArrayList<>();
+        // Collect the supported resolutions that are smaller than the preview Surface
+        List<Size> notBigEnough = new ArrayList<>();
+        int w = aspectRatio.getWidth();
+        int h = aspectRatio.getHeight();
+        for (Size option : choices) {
+            if (option.getWidth() <= maxWidth && option.getHeight() <= maxHeight &&
+                    option.getHeight() == option.getWidth() * h / w) {
+                if (option.getWidth() >= textureViewWidth &&
+                        option.getHeight() >= textureViewHeight) {
+                    bigEnough.add(option);
+                } else {
+                    notBigEnough.add(option);
+                }
+            }
+        }
+
+        // Pick the smallest of those big enough. If there is no one big enough, pick the
+        // largest of those not big enough.
+        if (bigEnough.size() > 0) {
+            return Collections.min(bigEnough, new CompareSizesByArea());
+        } else if (notBigEnough.size() > 0) {
+            return Collections.max(notBigEnough, new CompareSizesByArea());
+        } else {
+            MaoLog.getLogger("chooseOptimalSize").e("Couldn't find any suitable preview size");
+            return choices[0];
+        }
+    }
+
+    /**
+     * Compares two {@code Size}s based on their areas.
+     */
+    static class CompareSizesByArea implements Comparator<Size> {
+
+        @Override
+        public int compare(Size lhs, Size rhs) {
+            // We cast here to ensure the multiplications won't overflow
+            return Long.signum((long) lhs.getWidth() * lhs.getHeight() - (long) rhs.getWidth() * rhs.getHeight());
+        }
+
+    }
+
     private void startPreview(CameraDevice camera) {
         SurfaceTexture texture = ttv_camera.getSurfaceTexture();
-        texture.setDefaultBufferSize(ttv_camera.getWidth(), ttv_camera.getHeight());
+        // We configure the size of default buffer to be the size of camera preview we want.
+        texture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
         Surface surface = new Surface(texture);
         try {
             mPreviewBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
